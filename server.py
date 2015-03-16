83c406288c6548fdd0efeafa1b25c92e4f09ad4c
endlessproof <endlessproof@gmail.com>
2015-03-16 19:20:24 -0230

    Modify from basic to chatting server

--- server.py
+++ server.py
@@ -1,51 +1,89 @@
 #!/usr/bin/python3
 
+"""
+	1. If the master socket is readable, the server would accept the new connection.
+  2. If any of the client socket is readable, the server would read the message,
+     and broadcast it back to all clients except the one who send the message.
+"""
+
+import sys
+import select
 import time
 import socket
-#from tcpSocket import *
+# from tcpSocket import *
 
-if __name__ == '__main__':
-	"""
-		As a server
-		1. create socket(init)
-		2. bind adddress
-		3. listen on port
-		4. send/ recv message
-		5. shutdown
-		6. close
-	"""
-	hopCount = 5;
+HOPCOUNT = 10;
+SOCKET_LIST = [];			# [socket.socket], list of readable connection
+RECV_BUFFER = 4096
+HOST = '';
+PORT = 9999;
 
-	# get local machine host and port
-	host = socket.gethostname();
-	port = 9999;
+def broadcast(serversocket, sock, msg):
+	for so in SOCKET_LIST:
+		# send msg to peer only
+		if so != serversocket and so != sock:
+			try: so.send(msg);
+			except Exception as e:
+				# brocken socket connection
+				so.close();
+				if so in SOCKET_LIST: SOCKET_LIST.remove(so);
 
-	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-	print('Socket create.');
-	serversocket.bind((host, port));
-	print('Socket binding');
-	serversocket.listen(hopCount);
-	print('Socket listen: ', hopCount, ' hop')
-
-	while True:
-		clientsocket, address = serversocket.accept();
-		print('Got a connection from ', str(address));
-
-		currentTime = time.ctime(time.time()) + "\r\n";
-		clientsocket.send(currentTime.encode('ascii'));
-		clientsocket.close();
-		print('Socket close');
-		break;
+def chat_server():
 
 	# serversocket = tcpSocket();
-	# serversocket.bind(host, port);
-	# serversocket.listen(5);
+	# serversocket.bind(HOST, PORT);
+	# serversocket.listen(HOPCOUNT);
+	# SOCKET_LIST.append(serversocket.sock);	# Mark server socket is readable connection
 
-	# clientsocket, address = serversocket.sock.accept();	# return pair value (connection, addr)
-	# print('Got a connection from %s', str(address))
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+	print('Socket created.');
+	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1);
+	server_socket.bind((HOST, PORT));
+	print('Chatting server started on port ', PORT);
+	server_socket.listen(10);
+	print('Socket listen: ', HOPCOUNT, ' hop');
+	SOCKET_LIST.append(server_socket)
+
+	while True:
+		# Get the list sockets which are ready to be read thruogh select
+		# 4th argument, time_out = 0: poll and never block
+		ready_to_read, ready_to_write, in_error= select.select(SOCKET_LIST, [], [], 0);
+
+		for sock in ready_to_read:
+			# new connection request received
+			if sock == serversocket:
+				sockfd, address = serversocket.accept();
+				SOCKET_LIST.append(sockfd);
+				print('Client (%s, %s) connected'% address)
+				broadcast(serversocket, sockfd, "[%s:%s] entered our chatting room\n" % address)
+
+			# a message from client, not new connection
+			else:
+				try:
+					# receiving data from the socket
+					data = recv(RECV_BUFFER);
+					if data: broadcast(serversocket, sock, "\r["+ str(sock.getpeername())+ '] '+ data)
+					else:
+						# remove borcken socket
+						if sock in SOCKET_LIST: SOCKET_LIST.remove(sock);
+						# no data, means probably the connection has been brocken
+						broadcast(serversocket, sock, "Client (%s, %s) is offline\n" % address);
+				except Exception as e:
+					broadcast(serversocket, sock, "Client (%s, %s) is offline\n" % address);
+					continue;
+	serversocket.close();
 
 	# while True:
-	# 	currentTime = time.ctime(time.time()) + "\r\n";
-	# 	clientsocket.mysend(currentTime.encode('ascii'));
+	# 	# socket.accept() return pair value (connection, addr) to address
+	# 	clientsocket, address = serversocket.sock.accept();
+	# 	print('Got a connection from ', str(address));
+	# 	print(clientsocket)
 
-	# 	time.sleep(1);	# Sleep 1 sec.
+	# 	currentTime = time.ctime(time.time()) + "\r\n";
+	# 	clientsocket.send(currentTime.encode('ascii'));
+	# 	clientsocket.close();
+	# 	print('Socket close');
+	# 	break;
+
+if __name__ == '__main__':
+	sys.exit(chat_server());
