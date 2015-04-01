2a5d346b27130d8c7f8dc14236b38a1584eeea30
endlessproof <endlessproof@gmail.com>
2015-04-01 14:02:55 -0230

    ignore that

--- tcpSocket.py
+++ tcpSocket.py
@@ -31,22 +31,3 @@ class tcpSocket:
 	def connect(self, host, port):
 		self.sock.connect((host, port));
 		print('Socket connected.');
-
-	# def mysend(self, msg):
-	# 	total = 0;
-	# 	while total < MSGLEN:
-	# 		sent = self.sock.send(msg[total,]);
-	# 		if sent == 0: raise RuntimeError("socket connection broken");
-	# 		total += sent;
-	# 	print('Socket end of sending.');
-
-	# def myrecv(self): #if recv return 0 bytes, it means the connection is closed
-	# 	segment = [];
-	# 	bytes_recd = 0;
-	# 	while bytes_recd < MSGLEN:
-	# 		chunk = self.sock.recv(min(MSGLEN - bytes_recd, MSGLEN));
-	# 		if chunk == b'': raise RuntimeError("socket connection broken");
-	# 		segment.append(chunk);
-	# 		bytes_recd += len(chunk);
-	# 	print('Socket end of receiving');
-	# 	return b''.join(segment);
